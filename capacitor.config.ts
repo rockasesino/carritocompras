83d510a5f482af067ed0d780a12d341be2b0542c
Vivian Hincapie <carohinca1997@gmail.com>
2025-09-26 21:49:45 -0500

    Migración a Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.carolina.carrito',
+  appName: 'Carritoapp',
+  webDir: 'www'
+};
+
+export default config;
