e9b2fd9923039c7b119392faba82fb3ad88b059f
Vivian Hincapie <carohinca1997@gmail.com>
2025-09-26 23:41:29 -0500

    se añade idclient again

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.carolina.carrito',
   appName: 'Carritoapp',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SocialLogin: {
+      google: {
+        clientId: '706216325647-h14g47sl1vsk7b1d9upm1njtqruisce7.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+      },
+    },
+  }
 };
 
 export default config;
